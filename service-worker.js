9471e6eb3da5bfdd0dcaaeea099e164db71917dd
Merle Simmons III <61607754+LightningCog@users.noreply.github.com>
2021-03-01 11:23:02 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,3 +12,27 @@ self.addEventListener("install", e=>{
         })
     );
 });
+// Cache and return requests
+self.addEventListener("fetch", e=>{
+    e.respondWith(
+        caches.match(e.request).then(response=>{
+            return response || fetch(e.request);
+        })
+    );
+});
+
+// Update a service worker
+const cacheWhitelist = ['Simon-game'];
+self.addEventListener('activate', event => {
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
